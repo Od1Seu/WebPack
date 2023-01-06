e4bce0d5b09697e29a6f687a37eda652b7b2d173
Od1Seu <dimasobchenko.94@gmail.com>
2023-01-06 12:01:16 +0200

    add_loaders

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import Post from "./Post";
+import json from './assets/json'
+import Logo from './assets/webpack-logo.png'
+import xml from './assets/data.xml'
+import './styles/style.css'
 
-const new_post = new Post('Webpack title');
-console.log(new_post);
+const new_post = new Post('Webpack title', Logo);
+console.log('new_post:', new_post);
+console.log('Xml:', xml);
+console.log('Json:', json);
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'), // point from
   mode: 'development',
   entry: {
-    main: './src/index.js',
-	analytics: './src/analytics.js'
+    main: './index.js',
+	analytics: './analytics.js'
   },
   output: {
 	filename: "[name].[contenthash].js",
@@ -14,9 +15,28 @@ module.exports = {
   },
   plugins: [
     new HTMLWebpackPlugin({
-	  title: "WebPack title",
-	  template: "./src/index.html"
+	  template: "./index.html"
 	}),
 	new CleanWebpackPlugin()
-  ]
+  ],
+  module: {
+	rules: [
+	  {
+		test: /\.css$/,
+		use: ['style-loader', 'css-loader']
+	  },
+	  {
+		test: /\.(png|jpeg|svg)$/,
+		type: 'asset/resource'
+	  },
+	  {
+		test: /\.(ttf|woff|woff2|eot)/,
+		type: 'asset/resource'
+	  },
+	  {
+		test: /\.xml$/,
+		use: ['xml-loader']
+	  }
+	]
+  }
 };
